13a999f1f61875ce51e1c99941f4724afd66672e
AnimeshA867 <animeshacharya867+github@gmail.com>
2024-06-19 20:18:56 +0545

    For lab report upto unit 4

--- Unit 4/index.js
+++ Unit 4/index.js
@@ -1,4 +1,4 @@
-//Variable and Data types
+/* //Variable and Data types
 
 var x = 25; // This is not widely used but due hoisting property of the var.
 
@@ -16,4 +16,35 @@ const add = () => {
   let val = parseInt(first.value) + parseInt(last.value);
 
   result.innerText = val;
+};
+ */
+
+function getFactorial(n) {
+  if (n == 0) {
+    return 1;
+  } else {
+    return n * getFactorial(n - 1);
+  }
+}
+
+function fact() {
+  let num = prompt("Enter a number for factorial:");
+  let result = getFactorial(num);
+  alert(`The factorial of ${num} is ${result}`);
+}
+
+function getFibonacci(n) {
+  if (n == 0) {
+    return 0;
+  } else if (n == 1) {
+    return 1;
+  } else {
+    return getFibonacci(n - 1) + getFibonacci(n - 2);
+  }
+}
+
+const fibo = () => {
+  let num = prompt("Enter a number for fibonacci series:");
+  let result = getFibonacci(num);
+  alert(`The fibonacci of ${num} is ${result}`);
 };
